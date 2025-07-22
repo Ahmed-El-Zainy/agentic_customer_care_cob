655e26af4e61045545f26d60d1472800311c052a
Ahmed Mustafa <Ahmed_Mustafa_1251@fci.helwan.edu.eg>
2025-07-22 15:18:31 +0300

    add setup bash

---
+++ setup.sh
@@ -0,0 +1,9 @@
+"""
+GEMINI_API_KEY=your_gemini_api_key_here
+DATABASE_URL=postgresql://user:password@localhost:5432/chatbot_db
+REDIS_URL=redis://localhost:6379/0
+SECRET_KEY=your_secret_key_here
+ENVIRONMENT=development
+"""
+
+
